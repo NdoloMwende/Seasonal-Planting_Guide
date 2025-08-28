6500541cc985a5d936aca753330cde6269df4e7a
Alvin Muira <alvinmuira@gmail.com>
2025-08-28 23:36:54 +0300

    Home fetches crop Data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import { useState, useEffect } from 'react'
+
 function Home() {
+  // State for saving fetched Data
+  const[ cropData, setCropData ] = useState([])
+  // Fetching Data through useEffect
+  useEffect(() => {
+    fetch('http://localhost:3000/crops')
+      .then(response => response.json())
+      .then(data => setCropData(data))
+      .catch(error => console.error(error))
+  }, [])
+
   return (
     <div>
       <h1>Home Page</h1>
